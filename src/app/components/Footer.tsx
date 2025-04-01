0c880f1c91dba7ad7bf7d48a9923cc1470f78e4b
Carl0s2002 <99868670+Carl0s2002@users.noreply.github.com>
2025-04-01 13:18:34 +0300

    Added layout and profile picture

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="p-4 bg-gray-900 text-white text-center mt-10">
+        <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
